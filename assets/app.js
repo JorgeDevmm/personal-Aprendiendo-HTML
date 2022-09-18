40286f620b69b1242c6a49e8536e2f8e0bb31377
Jorge Monzon <jorge29luis10@gmail.com>
2022-09-18 01:47:39 -0500

    audio - video

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,21 @@
 console.log(`Aprendiendo a invocar código javascript`);
+
+function card() {
+  mensaje = `<article>
+        <article>
+          <img src="" alt="">
+          <p>pruebaLorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur explicabo at ea porro quis nemo distinctio ducimus blanditiis qui aut! Voluptatem vero modi optio laudantium ea dolorum consequatur, expedita rem.</p>
+        </article>
+    </article>`;
+
+  return mensaje;
+}
+const frase = card();
+console.log(frase);
+
+function Suma(a, b) {
+  return a + b;
+}
+
+const resultado = Suma(4, 5);
+console.log(resultado);
